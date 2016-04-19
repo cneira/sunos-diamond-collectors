a74df92a958879c95a63129672e9423f171d892b
Robert Fisher <slackboy@gmail.com>
2016-04-20 00:36:21 +0100

    initial commit of zones collector

---
+++ zones/zones.py
@@ -0,0 +1,32 @@
+# coding=utf8
+
+"""
+Zone Status Collector
+
+#### Dependencies
+
+ * /usr/sbin/zoneadm
+
+"""
+
+import diamond.collector
+import sunos_helpers
+
+class ZonesCollector(diamond.collector.Collector):
+
+    def get_default_config(self):
+        config = super(ZonesCollector, self).get_default_config()
+        config.update({
+            'path':   'zone',
+            'states': ['configured', 'down', 'incomplete',
+                       'installed', 'ready', 'running', 'shutting_down'
+                      ],
+            })
+        return config
+
+    def collect(self):
+        zones = sunos_helpers.run_cmd('/usr/sbin/zoneadm list -pc')
+        zones = [z.split(':')[2] for z in zones]
+
+        for state in self.config['states']:
+            self.publish('count.' + state, zones.count(state))
