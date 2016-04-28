1c13dcc0db6e5a8ca09852b00bac826e3076f4b2
Robert Fisher <slackboy@gmail.com>
2016-04-28 22:51:35 +0100

    fetch kstat

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/bin/ksh
 
+# A wrapper to start diamond in debug mode, using the config in this
+# directory. Also fetches the kstat module if you don't have it.
+
+if ! test -d kstat
+then
+    git clone https://github.com/pyhedgehog/kstat.git
+fi
+
 /opt/diamond/bin/diamond -f -l --skip-pidfile -c diamond-test.conf
