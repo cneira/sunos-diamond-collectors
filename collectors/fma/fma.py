8180eb1c9f770eda9e99d97d52675bd57086b5b6
Robert Fisher <slackboy@gmail.com>
2016-05-04 00:11:18 +0100

    refactor

--- collectors/fma/fma.py
+++ collectors/fma/fma.py
@@ -21,7 +21,7 @@ likely to be.
 """
 
 import diamond.collector
-import sunos_helpers
+import sunos_helpers as sh
 import re
 from collections import Counter
 
@@ -31,54 +31,64 @@ class FMACollector(diamond.collector.Collector):
         config = super(FMACollector, self).get_default_config()
         config.update({
             'path':     'fma',
-            'fmstat':   True,
+            'fmstat':   False,
             'fmadm':    True
             })
         return config
 
+    def fmadm(self):
+        try:
+            return sh.run_cmd('/usr/sbin/fmadm faulty', True)
+        except:
+            raise NotImplementedError('unable to run fmadm')
+
+    def fmstat(self):
+        try:
+            return sh.run_cmd('/usr/sbin/fmstat', True)
+        except:
+            raise NotImplementedError('unable to run fmstat')
+
+    def fmadm_impacts(self, fmadm_info):
+        impacts = filter(lambda l: re.search('Problem class', l),
+                fmadm_info)
+        return  [i.split(':')[1].strip() for i in impacts]
+
+    def collate_fmstats(self, fmstat_info):
+        """
+        return a dict of the form:
+
+        fmstat.module.key => value
+        """
+
+        keys = ('ev_recv', 'ev_acpt', 'wait', 'svc_t', 'pc_w',
+                'pc_b', 'open', 'solve', 'memsz', 'bufsz')
+
+        # first line is a header (there's no -H. Or -p. For shame.)
+        fmstat = self.fmstat()[1:]
+        ret = {}
+
+        for mod in fmstat:
+            # the first value, the module name will be part of the
+            # metric path
+            chunks = mod.split()
+            module = chunks.pop(0)
+            raw = zip(keys, chunks)
+
+            for k, v in raw:
+                if k == 'memsz' or k == 'bufsz':
+                    v = sh.bytify(v)
+
+                ret['fmstat.%s.%s' % (module, k)] = v
+
+        return ret
+
     def collect(self):
-        # This is an attempt to create a metric for alerting. It
-        # looks for any unresolved faults, and classifies them based
-        # on the first part of the failing FMRI
-
         if self.config['fmadm']:
-            try:
-                fmadm = sunos_helpers.run_cmd(
-                        '/usr/sbin/fmadm list-defect', True)
-            except Exception as e:
-                self.log.debug(e)
-                raise NotImplementedError('unable to run fmadm')
-
-            impacts = filter(lambda l: re.search('Impact', l), fmadm)
-            impacts = [i.split(':')[1].strip() for i in impacts]
+            impacts = self.fmadm_impacts(self.fmadm())
 
             for scheme, count in Counter(impacts).items():
                 self.publish('fmadm.alert.%s' % scheme, int(count))
 
         if self.config['fmstat']:
-            try:
-                fmstat = sunos_helpers.run_cmd('/usr/sbin/fmstat', True)
-            except:
-                raise NotImplementedError('unable to run fmstat')
-
-            keys = ('ev_recv', 'ev_acpt', 'wait', 'svc_t', 'pc_w', 'pc_b',
-                    'open', 'solve', 'memsz', 'bufsz')
-
-            # first line is a header (there's no -H. Or -p. For shame.)
-            fmstat = fmstat[1:]
-
-            for mod in fmstat:
-
-                # the first value, the module name will be part of the
-                # metric path
-
-                chunks = mod.split()
-                module = chunks.pop(0)
-
-                raw = zip(keys, chunks)
-
-                for k, v in raw:
-                    if k == 'memsz' or k == 'bufsz':
-                        v = sunos_helpers.bytify(v)
-
-                    self.publish('fmstat.%s.%s' % (module, k), v)
+            for k, v in self.collate_fmstats(self.fmstat()).items():
+                self.publish(k, v)
