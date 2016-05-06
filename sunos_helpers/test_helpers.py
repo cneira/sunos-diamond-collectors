e830c5be8db1081b348923e8751a41c97c3b2eb2
Robert Fisher <slackboy@gmail.com>
2016-05-06 23:19:26 +0100

    new helper library for tests

---
+++ sunos_helpers/test_helpers.py
@@ -0,0 +1,20 @@
+from os import path as p
+
+def read_fixture(includer, fixture):
+    """
+    Sometimes I can't use the supplied read fixture method because
+    the test chokes on "Mock not being iterable" or related. Also,
+    that method returns a string, and we are likely mocking the
+    output of `sunos_helpers.run_cmd()` which returns a list of
+    strings.
+
+    args:
+        includer: the file requesting a fixture must pass in its
+            name. Required. (string)
+        fixture: the name of the fixture file being requested.
+            Required. (string)
+    """
+
+    with open(p.join(p.dirname(p.realpath(includer)), 'fixtures',
+            fixture)) as fh:
+        return fh.read().strip().split('\n')
