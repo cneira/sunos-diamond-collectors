8e75ae28ecb3bbd43d63ce5b02380bdc3bd09cd0
Robert Fisher <slackboy@gmail.com>
2016-05-10 11:56:20 +0100

    use helper methods

--- collectors/sunos_nfs_client/sunos_nfs_client.py
+++ collectors/sunos_nfs_client/sunos_nfs_client.py
@@ -1,19 +1,19 @@
 import diamond.collector
-import sunos_helpers
+import sunos_helpers as sh
 
 class SunOSNFSClientCollector(diamond.collector.Collector):
 
     def get_default_config(self):
         config = super(SunOSNFSClientCollector, self).get_default_config()
         config.update({
-            'path':     'nfs.client',
+            'fields': '__all__',
+            'path':   'nfs.client',
             })
         return config
 
     def collect(self):
         for nfs_ver in self.config['nfs_vers']:
-            for k, v in sunos_helpers.kstat_name(
+            for k, v in sh.kstat_name(
                     'nfs::0::rfsreqcnt_v%s' % nfs_ver).iteritems():
-                if (not 'fields' in self.config) or (k in
-                self.config['fields']):
+                if sh.wanted(k, self.config['fields']):
                   self.publish('v%s.%s' % (nfs_ver, k), v)
