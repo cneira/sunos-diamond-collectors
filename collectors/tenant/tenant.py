6a8820d4315adf3d968eb44da764b7ce1faadb85
Robert Fisher <slackboy@gmail.com>
2016-05-17 10:36:38 +0000

    send most things as rates not counters

--- collectors/tenant/tenant.py
+++ collectors/tenant/tenant.py
@@ -36,3 +36,28 @@ class TenantCollector(diamond.collector.Collector):
                 if k == 'snaptime': continue
                 if k == 'value' and v > maxint * 2: continue
                 self.publish('%s.%s' % (kstat, k), v)
+
+        # I think nearly everything in the memory_cap kstat module
+        # should be considered a rate. Only `rss` and `swap` appear
+        # to be gauges.
+
+        no_deriv = ('rss', 'swap')
+
+        memcap = sh.kstat_raw_module('memory_cap')
+        snaptime = long(memcap['snaptime'])
+        del memcap['snaptime']
+
+        for stat in no_deriv:
+            self.publish('memory_cap.%s' % stat, memcap[stat])
+            del memcap[stat]
+
+        if 'memcap_snaptime' in self.last_values:
+            snaptime_delta = snaptime - self.last_values['memcap_snaptime']
+            self.log.debug('snaptime delta is %d' % snaptime_delta)
+
+            for k, v in memcap.items():
+                self.log.debug('memory_cap.%s = %d' % (k, v))
+                self.publish('memory_cap.%s' % k,
+                        self.derivative(k, v, time_delta=snaptime_delta))
+
+        self.last_values['memcap_snaptime'] = snaptime
