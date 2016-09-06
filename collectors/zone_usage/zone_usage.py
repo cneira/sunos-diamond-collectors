704430a3ba0fb607a84e4a4a54280a3efe05abc4
Robert Fisher <slackboy@gmail.com>
2016-09-06 17:52:40 +0100

    pretty much completely rewritten

--- collectors/zone_usage/zone_usage.py
+++ collectors/zone_usage/zone_usage.py
@@ -1,5 +1,12 @@
 import diamond.collector
 import sunos_helpers as sh
+import re
+
+"""
+This collector will produce metrics for all the zones on the box. It is
+kind of built on the assumption you have resource caps on all of those
+zones, because if you don't, many of the kstats will be meaningless.
+"""
 
 class ZoneUsageCollector(diamond.collector.Collector):
 
@@ -8,19 +15,98 @@ class ZoneUsageCollector(diamond.collector.Collector):
         config.update({
             'path':   'zone.usage',
             'zones':  '__all__',
+            'show_caps': False,
             })
         return config
 
-    def nprocs(self, zid):
-        # return the number of processes in the zone
-        return sh.get_kstat('caps:%s:nprocs_zone_%s:usage' %
-                (zid, zid)).values()[-1]
+    def kstats(self, zid):
+        return sh.get_kstat('caps:%s' % zid, no_times=True)
+
+    def process_table(self):
+        """
+        Use ps(1) to get a list of processes, zones (by ID), and
+        task ID
+        :returns: a dict where the key is the zone ID (cast to a
+            string) and the value is a dict of task_id =>
+            process_name. We don't bother with args.
+        """
+
+        raw = sh.run_cmd('/bin/ps -eo taskid,zoneid,comm')
+        raw.pop(0)
+
+        ret = {}
+
+        for l in raw:
+            task, zone, cmd = l.split()
+
+            if zone not in ret.keys():
+                ret[zone] = {}
+
+            ret[zone][task] = cmd.split('/')[-1]
+
+        return ret
+
+
+    def do_one_zone(self, zone, zid):
+        """
+        We're going to do a little massaging of the kstat names.
+        Primarily this will be removing the zone ID.
+
+        At the moment, we disregard projects. I don't use them.
+        usage
+        """
+
+        for stat, val in self.kstats(zid).items():
+
+            # Disregard projects. Maybe I'll implement them one day.
+
+            if '_project_' in stat or '_task_' in stat:
+                continue
+
+            if (stat.startswith('caps:%s:cpucaps_zone' % zid) and not
+                    stat.endswith('value')):
+                self.log.debug(stat)
+                self.publish('%s.cpucaps.%s' % (zone,
+                    stat.split(':')[-1]), val)
+
+            if (stat == 'caps:%s:swapresv_zone_%s:usage' % (zid, zid)):
+                self.publish('%s.memory.swapresv.usage' % zone, val)
+
+            if (stat == 'caps:%s:lockedmem_zone_%s:usage' % (zid, zid)):
+                self.publish('%s.memory.locked.usage' % zone, val)
+
+            if (stat == 'caps:%s:nprocs_zone_%s:usage' % (zid, zid)):
+                self.publish('%s.nprocs.usage' % zone, val)
+
+            if not self.config['show_caps']:
+                continue
+
+            if (stat == 'caps:%s:swapresv_zone_%s:value' % (zid, zid)):
+                self.publish('%s.memory.swapresv.value' % zone, val)
+
+            if (stat == 'caps:%s:lockedmem_zone_%s:value' % (zid, zid)):
+                self.publish('%s.memory.locked.value' % zone, val)
+
+            if (stat == 'caps:%s:nprocs_zone_%s:value' % (zid, zid)):
+                self.publish('%s.nprocs.value' % zone, val)
+
+            if (stat == 'caps:%s:cpucaps_zone_%s:value' % (zid, zid)):
+                self.publish('%s.cpucap.value' % zone, val)
+
+    def deal_with_tasks(self, tasks, zid):
+        for task in tasks:
+            self.log.debug(task)
+        #self.log.debug(self.tm[zid])
 
     def collect(self):
         zm = sh.zone_map(sh.zoneadm(), self.config['zones'])
+        #self.tm = self.process_table()
+
+        #zid = str(5)
+        #self.do_one_zone('shark-graylog', zid)
 
         # Our get_kstat() method doesn't handle wildcards (maybe it
         # should?) so we'll have to do a call for each zone.
 
         for zid, zone in zm.items():
-            self.publish('%s.nprocs' % zone, self.nprocs(zid))
+            self.do_one_zone(zone, zid)
